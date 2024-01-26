6a1dbc6f69bb0b1f349f41af5a48c7af00ea28a1
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-26 17:17:29 -0500

    init

---
+++ src/main/java/com/insurgencedev/rivalhoesboost/RivalHoesBoost.java
@@ -0,0 +1,17 @@
+package com.insurgencedev.rivalhoesboost;
+
+import com.insurgencedev.rivalhoesboost.listeners.RivalHoesEvenListener;
+import org.insurgencedev.insurgencesets.api.addon.ISetsAddon;
+import org.insurgencedev.insurgencesets.api.addon.InsurgenceSetsAddon;
+import org.insurgencedev.insurgencesets.libs.fo.Common;
+
+@ISetsAddon(name = "RivalHoesBoost", version = "1.0.0", author = "Insurgence Dev Team", description = "RivalHarvesterHoes Support")
+public class RivalHoesBoost extends InsurgenceSetsAddon {
+
+    @Override
+    public void onAddonReloadablesStart() {
+        if (Common.doesPluginExist("RivalHarvesterHoes")) {
+            registerEvent(new RivalHoesEvenListener());
+        }
+    }
+}
